920adf79dc94311eda389ba9acb2dab1bc491556
Bruno <stacheskibruno@gmail.com>
2021-02-24 00:13:03 -0300

    [ADD] Rota de criar surveys.

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
+import { SurveyController } from './controllers/SurveyController';
 
 const router = Router();
 
 const user = new UserController();
+const survey = new SurveyController();
 
 router.post('/users', user.create);
+router.post('/surveys', survey.create);
 
 export { router };
