105f3963ee5a38d6991a8aed59b203eaaaec1bd5
Bruno <stacheskibruno@gmail.com>
2021-02-24 00:35:38 -0300

    [ADD] Criado rota de mostrar todos os usuários e usuário por Uuid.

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,10 @@ const user = new UserController();
 const survey = new SurveyController();
 
 router.post('/users', user.create);
+router.get('/users/list', user.index)
+router.get('/users/:id', user.get, async (req, res) => {
+  res.send(req.params);
+})
 router.post('/surveys', survey.create);
 
 export { router };
