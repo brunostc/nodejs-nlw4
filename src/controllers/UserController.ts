4bde99e7464b1d0521b66d6ce2fd8d6cab795c93
Bruno <stacheskibruno@gmail.com>
2021-02-24 22:42:19 -0300

    [FIX] Criado repository de usuarios, modificado user controller.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 import { User } from '../models/User';
+import { UsersRepository } from '../repositories/UserRepository';
 
 class UserController {
 	async create(req: Request, res: Response) {
 		const { name, email } = req.body;
 
-		const usersRepository = getRepository(User);
+		const usersRepository = getCustomRepository(UsersRepository);
 
 		const userAlreadyExists = await usersRepository.findOne({ email });
 
@@ -40,3 +41,4 @@ class UserController {
 }
 
 export { UserController };
+
